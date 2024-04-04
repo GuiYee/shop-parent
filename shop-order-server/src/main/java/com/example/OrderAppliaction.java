0080e221e5463a8108fd959019db39eeda6365a6
liujun <jun.liu@51durian.com>
2024-04-04 23:46:03 +0800

    openFeign远程调用

--- shop-order-server/src/main/java/com/example/OrderAppliaction.java
+++ shop-order-server/src/main/java/com/example/OrderAppliaction.java
@@ -5,20 +5,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"com.example.model"})
+@EnableFeignClients
 public class OrderAppliaction {
 
     public static void main(String[] args) {
         SpringApplication.run(OrderAppliaction.class,args);
     }
 
-    @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate(){
-        return new RestTemplate();
-    }
 }
---
+++ shop-order-server/src/main/java/com/example/feign/ProductFeignApi.java
@@ -0,0 +1,20 @@
+package com.example.feign;
+
+
+import com.example.model.Product;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/*
+* 商品远程调用商品
+* */
+
+/*name 就是远程调用的服务名*/
+@FeignClient(name = "product-service",path = "/product")
+public interface ProductFeignApi {
+
+    //http://product-service/product/get?pid=1
+    @RequestMapping("/get")
+    Product get(@RequestParam("pid") Long pid);
+}
